839bd356d2dfd668ab206f7dcaf76c0a586524b7
alinegfchagas <alinegfchagas@hotmail.com>
2022-07-20 13:47:38 -0300

    estado global

--- pokedex7/src/pages/Pokedex/Pokedex.js
+++ pokedex7/src/pages/Pokedex/Pokedex.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+// import {pokeCard} from "../../Components/PokeCard/pokeCard";
+// import { goToDetailPage } from "../../Routes/Coordinator";
 
 export const Pokedex = () => {
     return (
         <div>
+
             <Link to={'/'}>
                 <button>Voltar</button>
             </Link>
